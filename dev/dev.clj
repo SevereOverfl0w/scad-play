74b52414a18d830f281efe480dcfc6146949c406
Dominic <dominic@juxt.pro>
2016-03-13 17:10:20 +0000

    Use reloaded.repl

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,29 +1,6 @@
 (ns dev
   (:require [com.stuartsierra.component :as component]
-            [clojure.tools.namespace.repl :refer (refresh)]
-            [clojure.stacktrace :refer [print-stack-trace]]
+            [reloaded.repl :refer [system init start stop go reset reset-all]]
             [app.core :as app]))
 
-(def system nil)
-
-(defn init []
-  (alter-var-root #'system
-    (constantly (app/new-system))))
-
-(defn start []
-  (try
-    (alter-var-root #'system component/start)
-    (catch Exception e
-      (print-stack-trace e))))
-
-(defn stop []
-  (alter-var-root #'system
-    (fn [s] (when s (component/stop s)))))
-
-(defn go []
-  (init)
-  (start))
-
-(defn reset []
-  (stop)
-  (refresh :after 'dev/go))
+(reloaded.repl/set-init! app/new-system)
